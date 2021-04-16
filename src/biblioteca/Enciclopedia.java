93e3604a1806935d7c4498cb91760d54621db9f9
Jcobos <jorcm9@gmail.com>
2021-04-16 14:30:07 +0200

    modifico el fichero

--- src/biblioteca/Enciclopedia.java
+++ src/biblioteca/Enciclopedia.java
@@ -1,5 +1,27 @@
 package biblioteca;
+//Este es el comentario
+public class Enciclopedia extends Libro {
 
-public class Enciclopedia {
+	
+	private String tematica;
+	
+	private String nivel_conocimientos;
+	
+	public static final String NIVEL_DEF="medio";
 
+	public String getTematica() {
+		return tematica;
+	}
+
+	public String getNivel_conocimientos() {
+		return nivel_conocimientos;
+	}
+	//CONSTRUCTOR
+	public Enciclopedia(int codigo, String titulo, int añoPublicacion, int numeroEjemplares, String tematica) {
+		super(codigo, titulo, añoPublicacion, numeroEjemplares);
+		this.tematica=tematica;
+		this.nivel_conocimientos=NIVEL_DEF;
+	}
+
+	//otro comentario mas
 }
